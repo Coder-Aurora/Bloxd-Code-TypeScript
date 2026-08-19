b3c8eec249881a2a00df731b6c41fc24e0d3e19e
Coder Aurora <CoderAurora@outlook.com>
2026-08-19 16:14:04 +0800

    Add circle function to create circular shapes

---
+++ Tools/Builder Tools/Circle.ts
@@ -0,0 +1,31 @@
+type Vector3 = [number, number, number];
+
+function circle(
+    centre: Vector3,
+    radius: number,
+    block: string = "White Wool"
+): void {
+    const [cx, cy, cz] = centre;
+    let
+        x: number = 0,
+        z: number = -radius;
+
+    while (x < -z) {
+        const mid: number = z + 0.5;
+
+        if (x * x + mid * mid - radius * radius)
+            z += 1;
+
+        api.setBlock(cx + x, cy, cz + z, block);
+        api.setBlock(cx + x, cy, cz - z, block);
+        api.setBlock(cx - x, cy, cz + z, block);
+        api.setBlock(cx - x, cy, cz - z, block);
+
+        api.setBlock(cx + z, cy, cz + x, block);
+        api.setBlock(cx + z, cy, cz - x, block);
+        api.setBlock(cx - z, cy, cz + x, block);
+        api.setBlock(cx - z, cy, cz - x, block);
+
+        x += 1;
+    }
+}
