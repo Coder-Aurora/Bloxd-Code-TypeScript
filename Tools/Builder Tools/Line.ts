5ce06f5582a3b61d7e3b9e66c2f0311a94a08f31
Coder Aurora <CoderAurora@outlook.com>
2026-08-19 16:02:18 +0800

    Implement line drawing function with delay

---
+++ Tools/Builder Tools/Line.ts
@@ -0,0 +1,42 @@
+// Why can't I use "Math" ????????????????????
+import { Async } from "./AsyncTimer";
+
+type Vector3 = [number, number, number];
+// Treat the symptoms but not the root cause...
+declare const Math: any;
+
+export function line(
+    pos1: Vector3,
+    pos2: Vector3,
+    block: string = "White Wool",
+    delay: number = 50
+): void {
+    const
+        [x1, y1, z1] = pos1,
+        [x2, y2, z2] = pos2;
+
+    const
+        dx: number = x2 - x1,
+        dy: number = y2 - y1,
+        dz: number = z2 - z1;
+
+    const maxStep: number = Math.max(Math.abs(dx), Math.abs(dy), Math.abs(dz));
+    if (maxStep === 0) {
+        api.setBlock(x1, y1, z1, block);
+        return;
+    }
+
+    let i: number = 0;
+    const loopId: number = Async.setIntervalLoop(() => {
+        const
+            curX: number = Math.round(x1 + (dx * i) / maxStep),
+            curY: number = Math.round(y1 + (dy * i) / maxStep),
+            curZ: number = Math.round(z1 + (dz * i) / maxStep);
+        api.setBlock(curX, curY, curZ, block);
+
+        ++i;
+        if (i > maxStep) {
+            Async.clearIntervalLoop(loopId);
+        }
+    }, delay);
+}
